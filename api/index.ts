200f3ba8de96e109841945d2c4daac333532ed61
guilherme <guilherme.l.nunes@gmail.com>
2020-10-19 23:46:30 -0300

    This commit adds basic structure for working with typescript and exress

---
+++ api/index.ts
@@ -0,0 +1,9 @@
+import express, { json } from 'express';
+
+const app = express();
+app.use(json());
+
+app.get('/', (request, response) => response.json({ message: 'Hello, TypeScript!'}));
+
+app.listen(3000, () => console.log('Server started on port 3000'));
+
