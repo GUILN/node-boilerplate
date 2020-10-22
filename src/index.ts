a5ad3f0f2b3cc1458af019ad690dce11301b867c
guilherme <guilherme.l.nunes@gmail.com>
2020-10-21 22:19:08 -0300

    This is the first commit (only tests are missing for this boilerplate)

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 import express, { json } from 'express';
+import { Item } from './domain/item';
 
 const app = express();
 app.use(json());
 
-app.get('/', (request, response) => response.json({ message: 'Hello, TypeScript!'}));
+app.get('/', (request, response) => response.json(new Item("Item")));
 
 app.listen(3000, () => console.log('Server started on port 3000'));
 
