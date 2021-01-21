12ba89e398225f945db38eca4c7af41c09b194ac
guiln <guilherme.l.nunes@gmail.com>
2021-01-21 00:27:02 -0300

    This commit refactors vote counter class

--- src/domain/entities/vote-counters/vote-counter.ts
+++ src/domain/entities/vote-counters/vote-counter.ts
@@ -1,5 +1,6 @@
 import { VoteParameter, OptionScore, DecisionParameters, VoteResult } from "../../vos/decision-value-objects";
 import ArchCriteria from "../arch-criteria";
+import ArchDecisionOption from "../arch-decision-option";
 import IVoteCounter from "./i-vote-counter";
 
 export default class VoteCounter implements IVoteCounter {
@@ -8,12 +9,12 @@ export default class VoteCounter implements IVoteCounter {
     }
     countVotesForCriteria(decisionParameters: DecisionParameters,computedVotes: VoteParameter[], criteria: ArchCriteria): VoteResult {
         let optionsScore: OptionScore[] = decisionParameters.options.map<OptionScore>(opt => ({option: opt, score: 0}));
-        let computeVoteForOption = (vote: VoteParameter): void => {
-            optionsScore.filter(optScore => optScore.option.name === vote.option.name)[0].score += 1;
+        let computeVoteForOption = (option: ArchDecisionOption): void => {
+            optionsScore.filter(optScore => optScore.option.name === option.name)[0].score += 1;
         }
 
         computedVotes.filter(cpVotes => cpVotes.criteria.name === criteria.name)
-                        ?.forEach(criteriaVote => computeVoteForOption(criteriaVote));
+                        ?.forEach(criteriaVote => computeVoteForOption(criteriaVote.option));
         return {optionsScore: optionsScore, isVotingConcluded: true};
     }
 }
